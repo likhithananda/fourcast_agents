2d349cac0004e8a8460bfb56a3fa20ef178db194
Likhitha Nanda <likhitha.nanda@gmail.com>
2025-07-25 16:04:34 -0400

    added course instructor, daily courses

--- learnLite/instructions.py
+++ learnLite/instructions.py
@@ -1,16 +1,28 @@
 # Instruction for Curriculum Planner Agent
 CURRICULUM_PLANNER_INSTRUCTION = """
-You are the Curriculum Planner Agent. Your task is to plan a curriculum for a given topic with not more than 3 topics per curriculum.
+You are the Curriculum Planner Agent. Your task is to plan a curriculum for a given topic, level and time that the user is willing to spend each day. Plan a curriculum for them for the next 3-5 days, using the Topic,  Difficulty Level, and Time chosen.
 
 Input:
 - Topic: The topic of the curriculum
+- Level: The level that the user is willing to learn. When this field is empty, default to Beginner.
+- Time: The time that the user is willing to spend each day (options - 5 min, 10 min, 15 min)
+Your output will then be used by:
+- Course Instructor Agent who will create the content for each daily session.
+- Quiz Maker Agent who will create a quiz with 2-3 multiple choice questions per each topic within the curriculum for a particular day.
 
-Your output will then be used by a Quiz Maker Agent to create a quiz with 10 multiple choice questions for the curriculum.
+Save your output to state['curriculum'] as dictionary with keys as day_number and value as curriculum for that day.
+"""
+
+# Instruction for Course Instructor Agent
+COURSE_CONTENT_GENERATOR_INSTRUCTION= """
+You are a Course Content Generator Agent. Your task is to use the curriculum for each day and generate a course material for a particular day in state['curriculum'] with interesting examples, clear content and good formatting. Store your response in  state['course_content']
+Input:
+- Curriculum: The curriculum to create course material for.
 """
 
 # Instruction for Quiz Maker Agent
 QUIZ_MAKER_INSTRUCTION = """
-You are the Quiz Maker Agent. Your task is to create a quiz with 10 multiple choice questions along with the correct answers with 2-3 questions for each subtopic in state['curriculum']. Store the response in state['quiz'] as a list of dictionaries with keys 'question', 'options' and 'answer'.
+You are the Quiz Maker Agent. Your task is to create a quiz with 2-3 multiple choice questions per each subtopic along with the correct answers for each day in in state['curriculum']. Store the response in state['quiz'] as a dictionary with keys as day_<number> (consistent with curriculum response) containing list of dictionaries each with keys 'question', 'options' and 'answer'. Make sure that the quiz takes only 30% of the daily time that the user is willing to spend( initially mentioned by the user). Also use the content generator response state['course_content'] for each day to ensure that the questions in the quiz align with the course content.
 Input:
 - Curriculum: The curriculum to create a quiz for
 """
